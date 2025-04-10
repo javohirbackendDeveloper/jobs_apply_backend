3ef6e5a97ed682b3176b011c42c6b2a438702d52
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-04-10 17:19:10 +0500

    d

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   await app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on ${PORT}`);
   });
